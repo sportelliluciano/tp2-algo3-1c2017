0dd97093c7b1ede71980438872e4f0749c3d6493
EmiSor <emilianosorbello@gmail.com>
2017-06-03 01:50:55 -0300

    adding basic test class

--- test/main/TestMain.java
+++ test/main/TestMain.java
@@ -1,8 +1,13 @@
 package main;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestMain {
+	
+	@Test
 	public void TestNothing() {
-		
-		return;
+		assertTrue(true);
 	}
 }
