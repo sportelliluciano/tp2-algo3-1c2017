45cc162d21755b5f1e4521821368306ea2c28a31
EmiSor <emilianosorbello@gmail.com>
2017-06-03 02:33:33 -0300

    STUBS para los test que deben pasar en la primera semana

---
+++ test/integration/TestFirstWeek.java
@@ -0,0 +1,73 @@
+package integration;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestFirstWeek {
+	
+	@Test
+	public void test01MoveCharacterAndCheckNewPosition(){
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	
+	@Test
+	public void test02ZWarriorCantMoveThroughAnotherZWarrior() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	@Test
+	public void test02ZWarriorCantMoveThroughEnemyOfTheEarth() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	@Test
+	public void test02EnemyOfTheEarthCantMoveThroughAnotherEnemyOfTheEarth() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	@Test
+	public void test02EnemyOfTheEarthCantMoveThroughZWarrior() {
+		// TODO: Implement
+		assertTrue(false);
+	}	
+	@Test
+	public void test03CheckIfCharacterTransformed() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	@Test
+	public void test04MoveCharacterAfterTransform() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	@Test
+	public void test05MoveCharcterAfterTransform() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	@Test
+	public void test06CreateAGameCheckCharacterPositions() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	@Test
+	public void test07AttackingEnemyTooFarAwayDoesntDamageIt() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+	@Test
+	public void test07AttackingEnemyCloseEnoughDamagesIt() {
+		// TODO: Implement
+		assertTrue(false);
+	}
+	
+}
