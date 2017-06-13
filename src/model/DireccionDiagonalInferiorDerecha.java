71cab468dbd0bb046e6c1230f7b5434afd7f7f1d
Camilo <Camilo@Blackbird>
2017-06-13 09:10:18 -0300

    Agregue Diagonales

--- src/model/DireccionDiagonalInferiorDerecha.java
+++ src/model/DireccionDiagonalInferiorDerecha.java
@@ -5,7 +5,7 @@ import model.error.ErrorPosicionInvalida;
 public class DireccionDiagonalInferiorDerecha extends Direccion{
 	
 	public Posicion obtenerPosicionNueva(Posicion actual) throws ErrorPosicionInvalida {
-		return new Posicion(actual.getX() + 1, actual.getY() + 1);
+		return new Posicion(actual.getX() + 1, actual.getY() + 1); // Devuelve la posicion corrida en el sentido indicado
 	}
 
 }
