f68adf22d920bef0db73c5079939920085768998
Camilo <Camilo@Blackbird>
2017-06-13 07:37:52 -0300

    Agregue los 4 versores diagonales.

--- src/model/DireccionDiagonalInferiorIzquierda.java
+++ src/model/DireccionDiagonalInferiorIzquierda.java
@@ -2,10 +2,10 @@ package model;
 
 import model.error.ErrorPosicionInvalida;
 
-public class DireccionDiagonalSuperiorIzquierda extends Direccion{
+public class DireccionDiagonalInferiorIzquierda extends Direccion{
 	
 	public Posicion obtenerPosicionNueva(Posicion actual) throws ErrorPosicionInvalida {
-		return new Posicion(actual.getX() - 1, actual.getY() - 1);
+		return new Posicion(actual.getX() -1, actual.getY() + 1);
 	}
 
 }
