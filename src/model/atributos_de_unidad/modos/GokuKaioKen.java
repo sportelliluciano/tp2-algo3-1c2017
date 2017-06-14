2c5fbfcd8adeb54fbf04484d8c782127ee1640ff
jona-gg <manson_static@hotmail.com>
2017-06-14 02:42:59 +0000

    GokuKaioKen.java edited online with Bitbucket

--- src/model/atributos_de_unidad/modos/GokuKaioKen.java
+++ src/model/atributos_de_unidad/modos/GokuKaioKen.java
@@ -5,18 +5,23 @@ import model.atributos_de_unidad.Modo;
 
 public class GokuKaioKen extends Modo {
 
+	private int costoKi = 50;
+	private Modo siguienteModo = new GokuSSJ();	
 	private int velocidad = 3;
 
 	@Override
 	public boolean puedeTransformarse(Unidad u) {
-		return false;
+		return u.getKi().getMagnitud() >= this.costoKi;
 	}
 
 	@Override
-	public Modo transformarA(Unidad u) {
-		return new GokuSSJ();
+	public Modo transformarA(Unidad u) throws ErrorNoCumpleReqTrans {
+		if (!puedeTransformarse(u))
+			throw new ErrorNoCumpleReqTrans();
+		//u.getKi().reducirEn(this.costoKi);
+		return this.siguienteModo;
 	}
-	
+
 	@Override
 	public String getNombre() {
 		return "Kaio-ken";
@@ -24,6 +29,6 @@ public class GokuKaioKen extends Modo {
 
 	@Override
 	public int getVelocidad() {
-		return velocidad ;
+		return velocidad;
 	}
 }
