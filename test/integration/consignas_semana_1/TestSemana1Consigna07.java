363e1413db4d206cb86953fa8e5af9f17168b23d
jona-gg <manson_static@hotmail.com>
2017-06-17 06:36:34 +0000

    TestSemana1Consigna07.java edited online with Bitbucket

--- test/integration/consignas_semana_1/TestSemana1Consigna07.java
+++ test/integration/consignas_semana_1/TestSemana1Consigna07.java
@@ -1,19 +1,53 @@
 package integration.consignas_semana_1;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import model.Posicion;
+import model.Tablero;
+import model.Unidad;
+import model.atributos_de_unidad.modos.GokuNormal;
+import model.error.ErrorEnemigoFueraDeAlcance;
+import model.error.ErrorPosicionInvalida;
+
 public class TestSemana1Consigna07 {
-	@Test
-	public void test07NoSePuedeAtacarEnemigosMuyLejos() {
-		// TODO: Implement
-		assertTrue(false);
-	}
+	
+	@Test(expected = ErrorEnemigoFueraDeAlcance.class)  
+	public void test07NoSePuedeAtacarEnemigosMuyLejos() throws ErrorEnemigoFueraDeAlcance, ErrorPosicionInvalida {
+		
+		Tablero tablero = new Tablero(20,20);
+		Unidad goku = new Unidad(new GokuNormal());
+		Unidad freezer = new Unidad(new FreezerNormal());
+		
+		tablero.agregarUnidad(goku, new Posicion(3,3));//distancia de ataque = 2
+		tablero.agregarUnidad(freezer, new Posicion(15,15));//se encuentra a distancia = 12
+
+		goku.atacarBasicoA(freezer);
+		
+	}		
 	
 	@Test
-	public void test07AtacarEnemigoCercanoReduceSuVida() {
-		// TODO: Implement
-		assertTrue(false);
+	public void test07AtacarEnemigoCercanoReduceSuVida()throws ErrorEnemigoFueraDeAlcance, ErrorPosicionInvalida  {
+		Tablero tablero = new Tablero(20,20);
+		
+		Unidad goku = new Unidad(new GokuNormal());//500 puntos de vida y poder de pelea = 20
+		goku.setVida(500);
+
+		Unidad freezer = new Unidad(new FreezerNormal());//400 puntos de vida
+		freezer.setVida(400);
+		
+		Posicion posGoku =  new Posicion(3,3),posFreezer = new Posicion(4,4);
+		
+		tablero.agregarUnidad(goku,posGoku);//distancia de ataque = 2
+		tablero.agregarUnidad(freezer,posFreezer );//se encuentra a distancia = 1
+		
+		int vidaOponenteAntesDeAtaque = freezer.getVida();
+		
+		goku.atacarBasicoA(freezer);
+		
+		assertEquals(freezer.getVida(),(vidaOponenteAntesDeAtaque - goku.getAtaqueBasico()));
+		
 	}
+	
 }
