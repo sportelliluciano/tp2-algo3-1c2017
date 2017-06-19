b8dcbb13b51bbb4b7434fc61bf385616feace348
JonaGG <manson_static@hotmail.com>
2017-06-19 16:10:38 -0300

    Update TestSemana1Consigna06.java
    modifique la prueba totalmente igualmente antes lo habia hecho yo pero no esta bien hecho,ahora pasa la prueba,pero se modifico jugador para que ande.Nota:no quedo prolijo digamos,pero anda,después fijensen,creo q la logica de las esquinas esta bien.

--- test/integration/consignas_semana_1/TestSemana1Consigna06.java
+++ test/integration/consignas_semana_1/TestSemana1Consigna06.java
@@ -2,76 +2,94 @@ package integration.consignas_semana_1;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.Vector;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 import model.Juego;
 import model.Jugador;
+import model.Posicion;
 import model.Tablero;
-import model.Unidad;
+import model.error.ErrorPosicionInvalida;
+import model.personajes.Cell;
+import model.personajes.Freezer;
+import model.personajes.Gohan;
+import model.personajes.Goku;
+import model.personajes.MajinBoo;
+import model.personajes.Piccolo;
 	
 //No se si esta del todo bien,pero lo pongo para ver si sirve,faltaria el elegir unidades
 //en jugador para que pueda devolverlas y asi poder saber las posiciones 
 public class TestSemana1Consigna06 {	
 	@Test
-	public void test06CrearJuegoYValidarPosicionesDeTablero() {
+	public void test06CrearJuegoYValidarPosicionesDeTablero() throws ErrorPosicionInvalida {		
 		Jugador jugador1,jugador2;
 		jugador1 = new Jugador("Fabio");
-		jugador2 = new Jugador("Marito"); // Quien es Marito? XD
+		jugador2 = new Jugador("Marito");
 
 		Juego juego = new Juego(jugador1, jugador2);
 		
-		assertTrue(false);
-		/*
+		jugador1.setJuego(juego,null);//como es el jugador 1 no tiene esquina rival,entonces paso null
+		jugador2.setJuego(juego,jugador1.getEsquina());
+		
 		Tablero tablero = juego.getTablero();
 		
 		int alto = tablero.getAlto();
 		int ancho = tablero.getAncho();
 		
-		// No entiendo que representan estas esquinas, la verdad
-		int[][] esquinaSuperiorIzq ={{1,1},{2,1},{1,2}};	
-		int[][] esquinaSuperiorDer ={{1,ancho},{2,ancho - 1},{1,ancho}};		
-		int[][] esquinaInferiorIzq ={{alto,1},{alto,2},{alto - 1,1}};		
-		int[][] esquinaInferiorDer ={{alto,ancho - 1},{alto - 1,ancho},{alto,ancho}};				
-				
-		int[][] esquinaJugador1 = null, esquinaJugador2 = null;
+		jugador1.elegirPersonaje(new Gohan());
+		jugador1.elegirPersonaje(new Goku());
+		jugador1.elegirPersonaje(new Piccolo());
+
+		jugador2.elegirPersonaje(new Freezer());
+		jugador2.elegirPersonaje(new Cell());
+		jugador2.elegirPersonaje(new MajinBoo());
+
+		Set<Posicion> esquina1 = jugador1.getEsquina();
 		
-		Vector<Unidad> unidades1,unidades2;
-		unidades1 = jugador1.getUnidades(); // <- Esto no se si me gusta
-		unidades2 = jugador2.getUnidades(); // <- Esto no se si me gusta
+		Set<Posicion> esquina2 = jugador2.getEsquina();
 		
-		for(int i=0; i < 3; i++){
-			esquinaJugador1[i][0] = unidades1.get(i).getPosicion().getX();
-			esquinaJugador1[i][1] = unidades1.get(i).getPosicion().getY();
-			
-			esquinaJugador2[i][0] = unidades1.get(i).getPosicion().getX();
-			esquinaJugador2[i][1] = unidades1.get(i).getPosicion().getY();
-		}
-			
-		if( esquinasSonIguales(esquinaJugador1,esquinaSuperiorIzq))
-			assertTrue(esquinasSonIguales(esquinaJugador2,esquinaInferiorDer));
-	
-		if( esquinasSonIguales(esquinaJugador1,esquinaSuperiorDer) )
-			assertTrue(esquinasSonIguales(esquinaJugador2,esquinaInferiorIzq));
-		    
-		if( esquinasSonIguales(esquinaJugador1,esquinaInferiorIzq) )
-			assertTrue(esquinasSonIguales(esquinaJugador2,esquinaSuperiorDer));
-		    
-		if( esquinasSonIguales(esquinaJugador1,esquinaInferiorDer) )
-			assertTrue(esquinasSonIguales(esquinaJugador2,esquinaSuperiorIzq));
-		*/
+		//preparo las esquinas del tablero para poder compararlas con las de los jugadores
+		
+        Set<Posicion> esquinaSuperiorIzq = new HashSet<Posicion>();
+		
+		esquinaSuperiorIzq.add(new Posicion(0,0));
+		esquinaSuperiorIzq.add(new Posicion(1,0));
+		esquinaSuperiorIzq.add(new Posicion(0,1));
+		
+		Set<Posicion> esquinaSuperiorDer= new HashSet<Posicion>();
+		
+		esquinaSuperiorDer.add(new Posicion(0,alto));
+		esquinaSuperiorDer.add(new Posicion(1,alto));
+		esquinaSuperiorDer.add(new Posicion(0,alto - 1));
+		
+		Set<Posicion> esquinaInferiorDer = new HashSet<Posicion>();
+
+		esquinaInferiorDer.add(new Posicion(ancho,alto - 1));
+		esquinaInferiorDer.add(new Posicion(ancho - 1,alto));
+		esquinaInferiorDer.add(new Posicion(ancho,alto));
+
+		Set<Posicion> esquinaInferiorIzq = new HashSet<Posicion>();
+
+		esquinaInferiorIzq.add(new Posicion(ancho,0));
+		esquinaInferiorIzq.add(new Posicion(ancho,1));
+		esquinaInferiorIzq.add(new Posicion(ancho - 1,0));
+
+	//comparo que cada jugador tenga su esquina y q el otro la opuesta
+		
+		if( esquina1.containsAll(esquinaSuperiorIzq))
+			assertTrue(esquina2.containsAll(esquinaInferiorDer));
+		else
+		if( esquina1.containsAll(esquinaSuperiorDer) )
+			assertTrue(esquina2.containsAll(esquinaInferiorIzq));
+		else    
+		if( esquina1.containsAll(esquinaInferiorIzq) )
+			assertTrue(esquina2.containsAll(esquinaSuperiorDer));
+		else    
+		if( esquina1.containsAll(esquinaInferiorDer) )
+			assertTrue(esquina2.containsAll(esquinaSuperiorIzq));
+		
 	}
-	
-	private boolean esquinasSonIguales(int[][] esquina1,int[][] esquina2){
-		int i;
-		
-		for(i=0; i < 3; i++){
-			for(int k=0;k < 3;k++)
-		        if( esquina1[i][0] == esquina2[k][0])
-		        	if(esquina1[i][0] == esquina2[k][1])
-			            i++;
-		}	
-		return i==3;//las tres posiciones son iguales
-   }
+
 }
