e0c0ddd0fd52b0c276bca0e920f1cf80a62aa959
JonaGG <manson_static@hotmail.com>
2017-06-20 00:48:23 -0300

    Update TestSemana1Consigna04.java
    reparado test 4

--- test/integration/consignas_semana_1/TestSemana1Consigna04.java
+++ test/integration/consignas_semana_1/TestSemana1Consigna04.java
@@ -32,12 +32,21 @@ public class TestSemana1Consigna04 {
 		goku.transformarse();
 	}
 	
-	/*
 	@Test (expected = ErrorNoHayMasTrans.class)
 	public void test04bTransformarseEstandoEnUltimaTransformacionLanzaError() throws ErrorNoCumpleReqTrans, ErrorPosicionInvalida, ErrorNoHayMasTrans {
-		Unidad goku = new Unidad(new GokuSSJ());
+		Unidad goku = new Goku(null);	
+		for (int i = 0; i < 4; i++) // Goku requiere 20 ki para el kaioken y 
+			goku.pasarTurno();		// carga 5 ki por turno.
 		
-		goku.transformarse();		
-	}*/
+		goku.transformarse();
+		
+		for (int i = 0; i < 10; i++) // Goku requiere 50 ki para el SSJ y 
+			goku.pasarTurno();		// carga 5 ki por turno.
+		
+		goku.transformarse();
+		
+		goku.transformarse();// no hay mas transformaciones		
+
+	}
 	
 }
