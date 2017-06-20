d43b7ea6cb2fd1c3874a18749aae12b9d28f13c6
JonaGG <manson_static@hotmail.com>
2017-06-20 01:41:33 -0300

    Create TestSemana2Consigna01.java

---
+++ test/integration/consignas_semana_2/TestSemana2Consigna01.java
@@ -0,0 +1,38 @@
+package integration.consignas_semana_2;
+
+import org.junit.Test;
+
+import model.Posicion;
+import model.Tablero;
+import model.Unidad;
+import model.equipos.GuerrerosZ;
+import model.error.ErrorNoCumpleReqTrans;
+import model.error.ErrorNoHayMasTrans;
+import model.error.ErrorPosicionInvalida;
+
+public class TestSemana2Consigna01 {
+	
+	@Test(expected = ErrorNoCumpleReqTrans.class)
+	public void test04aTransformarseCambiaElModo() throws ErrorNoCumpleReqTrans, ErrorNoHayMasTrans, ErrorPosicionInvalida {
+		Tablero tablero = new Tablero(20,20);
+		GuerrerosZ guerreros = new GuerrerosZ();
+		
+		Unidad gohan = guerreros.getGohan(),goku = guerreros.getGoku(), piccolo = guerreros.getPiccolo();	
+		
+		tablero.agregarUnidad(gohan, new Posicion(3,3));
+		tablero.agregarUnidad(goku, new Posicion(3,4));
+		tablero.agregarUnidad(piccolo, new Posicion(4,3));
+
+		for (int i = 0; i < 2; i++) // Gohan requiere 10 ki para SSJ1 y 
+			gohan.pasarTurno();		// carga 5 ki por turno.
+		
+		gohan.transformarse();
+		
+		for (int i = 0; i < 6; i++) // Gohan requiere 30 ki para SSJ2 y 
+			gohan.pasarTurno();		// carga 5 ki por turno.
+		
+		gohan.transformarse();//no puede transformar porque necesita que sus aliados tengan menod de 30% vida
+		
+	}
+	
+}
