361fff4fa001e78fd23d0dd3502fdb65bf8e52ed
JonaGG <manson_static@hotmail.com>
2017-06-20 01:57:23 -0300

    Update TestSemana2Consigna02.java

--- test/integration/consignas_semana_2/TestSemana2Consigna02.java
+++ test/integration/consignas_semana_2/TestSemana2Consigna02.java
@@ -16,7 +16,7 @@ import model.error.ErrorPosicionInvalida;
 public class TestSemana2Consigna02 {
 
 	@Test
-	public void test02TransformarGohanAultilmaTransformacionConCompañerosDeVidaInferiorAlTreintaPorcientoCambiaElModo() throws ErrorNoCumpleReqTrans, ErrorNoHayMasTrans, ErrorPosicionInvalida {
+	public void test02TransformarGohanAultilmaTransformacionConAliadosDeVidaInferiorAlTreintaPorcientoCambiaElModo() throws ErrorNoCumpleReqTrans, ErrorNoHayMasTrans, ErrorPosicionInvalida {
 		Tablero tablero = new Tablero(20,20);
 		GuerrerosZ guerreros = new GuerrerosZ();
 		
