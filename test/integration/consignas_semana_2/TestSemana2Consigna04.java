298124a4d1ee376b375ebeb076ed8d513803ab5d
JonaGG <manson_static@hotmail.com>
2017-06-20 03:00:55 -0300

    Update TestSemana2Consigna04.java

--- test/integration/consignas_semana_2/TestSemana2Consigna04.java
+++ test/integration/consignas_semana_2/TestSemana2Consigna04.java
@@ -14,27 +14,27 @@ import model.error.ErrorPosicionInvalida;
 public class TestSemana2Consigna04 {
 	
   	public void test04TransformarPiccoloAprotectorConGohanConMenosDeVeintePorcientoDeVidaCambiaModo() throws ErrorNoCumpleReqTrans, ErrorNoHayMasTrans, ErrorPosicionInvalida {
-	    	Tablero tablero = new Tablero(20,20);
-		    GuerrerosZ guerreros = new GuerrerosZ();
+	        Tablero tablero = new Tablero(20,20);
+	        GuerrerosZ guerreros = new GuerrerosZ();
 		
-		    Unidad gohan = guerreros.getGohan(),goku = guerreros.getGoku(), piccolo = guerreros.getPiccolo();	
+	        Unidad gohan = guerreros.getGohan(),goku = guerreros.getGoku(), piccolo = guerreros.getPiccolo();	
 		
-	    	tablero.agregarUnidad(gohan, new Posicion(3,3));
-		    tablero.agregarUnidad(goku, new Posicion(3,4));
-		    tablero.agregarUnidad(piccolo, new Posicion(4,3));
+	        tablero.agregarUnidad(gohan, new Posicion(3,3));
+	        tablero.agregarUnidad(goku, new Posicion(3,4));
+	        tablero.agregarUnidad(piccolo, new Posicion(4,3));
 
-		    for (int i = 0; i < 4; i++) // Piccolo requiere 20 ki para SSJ1 y 
-			      piccolo.pasarTurno();		// carga 5 ki por turno.
+	        for (int i = 0; i < 4; i++) // Piccolo requiere 20 ki para SSJ1 y 
+		       piccolo.pasarTurno();		// carga 5 ki por turno.
 		
-		    piccolo.transformarse();
+         	piccolo.transformarse();
 		
-	      AtaqueBasico ataque = new AtaqueBasico(270);
+	        AtaqueBasico ataque = new AtaqueBasico(270);
 		
-		    gohan.recibirAtaque(ataque);//tiene 300 de vida entonces lo dejo con 30 puntos(10%)
+                gohan.recibirAtaque(ataque);//tiene 300 de vida entonces lo dejo con 30 puntos(10%)
 		
-		    piccolo.transformarse();
+	        piccolo.transformarse();
 		
-		    assertTrue(gohan.getModo().getNombre().equals("Piccolo Protector"));
+	        assertTrue(gohan.getModo().getNombre().equals("Piccolo Protector"));
 
 	}
 	
