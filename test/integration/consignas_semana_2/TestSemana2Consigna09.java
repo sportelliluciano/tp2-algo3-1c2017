a89d004eb897ad51e1ae3069db89ce7dd099f79b
JonaGG <manson_static@hotmail.com>
2017-06-21 01:07:14 -0300

    Create TestSemana2Consigna09.java
    hay que ver si el daño necesario es menor al 30 o 20 %,el enunciado se contradice

---
+++ test/integration/consignas_semana_2/TestSemana2Consigna09.java
@@ -0,0 +1,41 @@
+package integration.consignas_semana_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Posicion;
+import model.Tablero;
+import model.equipos.EnemigosDeLaTierra;
+import model.equipos.GuerrerosZ;
+import model.error.ErrorEnemigoFueraDeAlcance;
+import model.error.ErrorPosicionInvalida;
+import model.error.ErrorUnidadNoEsEnemiga;
+import model.personajes.Freezer;
+import model.personajes.Goku;
+
+public class TestSemana2Consigna09 {
+
+	@Test
+	public void test09GokuAumentaDañoAlReducirVidaDeBajoAlVeintePorciento() throws ErrorPosicionInvalida, ErrorUnidadNoEsEnemiga, ErrorEnemigoFueraDeAlcance {
+    Tablero tablero = new Tablero(19,23);
+		
+		GuerrerosZ guerreros = new GuerrerosZ();
+		Goku goku = guerreros.getGoku();
+		tablero.agregarUnidad(goku,new Posicion(5,5));
+		
+		EnemigosDeLaTierra enemigos = new EnemigosDeLaTierra();
+		Freezer freezer = enemigos.getFreezer();//la vida de freezer es de 400
+		tablero.agregarUnidad(freezer, new Posicion(5,6));
+		
+		for(int i = 0; i <= 20; i++ )
+		     freezer.ataqueBasicoA(goku, tablero);//saca 20 de vida
+        //goku queda con menos de 20% de vida	
+		
+		goku.ataqueBasicoA(freezer, tablero);
+				
+		assertEquals(freezer.getVidaActual() , 376 );//aumenta un 20% entonces es 24 el daño que hace goku
+		
+	}
+
+}
