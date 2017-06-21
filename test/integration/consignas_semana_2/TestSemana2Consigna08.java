65815a114a35f6d2057ce8badeb5b8ef2ee705be
JonaGG <manson_static@hotmail.com>
2017-06-20 23:39:39 -0300

    Create TestSemana2Consigna08.java
    esta prueba explota porque hay que reparar el tema del ki,cuando se transforma o gaste ki para ataque especial,debe perder ki,ademas cuando majin boo tira este ataque ,debe no poder incrementar su ki durante 3 turnos.

---
+++ test/integration/consignas_semana_2/TestSemana2Consigna08.java
@@ -0,0 +1,48 @@
+package integration.consignas_semana_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Posicion;
+import model.Tablero;
+import model.equipos.EnemigosDeLaTierra;
+import model.equipos.GuerrerosZ;
+import model.error.ErrorEnemigoFueraDeAlcance;
+import model.error.ErrorKiInsuficiente;
+import model.error.ErrorPosicionInvalida;
+import model.error.ErrorUnidadNoEsEnemiga;
+import model.personajes.Cell;
+import model.personajes.Goku;
+import model.personajes.MajinBoo;
+
+public class TestSemana2Consigna08 {
+
+	@Test
+	public void test08AtacarConMajinBooUsandoAtaqueEspecialConvierteEnChocolate() throws ErrorPosicionInvalida, ErrorUnidadNoEsEnemiga, ErrorKiInsuficiente, ErrorEnemigoFueraDeAlcance {
+
+    Tablero tablero = new Tablero(19,23);
+		
+		GuerrerosZ guerreros = new GuerrerosZ();
+		Goku goku = guerreros.getGoku();
+		tablero.agregarUnidad(goku,new Posicion(5,5));
+		
+		EnemigosDeLaTierra enemigos = new EnemigosDeLaTierra();
+		MajinBoo majinBoo = enemigos.getMajinBoo();
+		tablero.agregarUnidad(majinBoo, new Posicion(5,6));
+		
+		for(int i = 0; i < 6; i++)
+			  majinBoo.pasarTurno();//necesita 30 de ki para ataqueEspecial
+			
+	  majinBoo.ataqueEspecialA(goku, tablero);
+	
+    assertEquals(goku.getEstado().duracionParalisis,3);
+	    
+	  for(int i = 0 ;i < 3; i++){//majinBoo no carga su ki durante 3 tiempos
+	    	majinBoo.pasarTurno();
+	      assertEquals(majinBoo.getKi().getMagnitud(),0);
+	  }
+    
+	}
+
+}
